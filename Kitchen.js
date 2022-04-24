e0593067db95c23184470365a49265ad862343fc
surajsudhi <suraj.sudhi@gmail.com>
2022-04-24 18:08:01 +0530

    Add files via upload

---
+++ Kitchen.js
@@ -0,0 +1,32 @@
+img=""
+Status = ""
+
+function preload(){
+    img = loadImage('kitchen.jpeg');
+    }
+
+function setup(){
+    canvas = createCanvas(640,420);
+    canvas.center();
+    ObjectDetection = ml5.objectDetector('cocossd', modelLoaded);
+    Status = document.getElementById("status").innerHTML = "Detecting objects...";
+    }
+
+    function draw(){
+    image(img, 0, 0 , 640, 420);
+    }
+
+    function modelLoaded(){
+    console.log("Model loaded");
+     Status = true
+     ObjectDetection.detect(img, gotResult);
+     }
+
+     function gotResult(error,result){
+        if(error){
+        console.error(error)    
+        }
+        else{
+        console.log(result)    
+        }
+        }
---
+++ main.js
@@ -0,0 +1,11 @@
+function bedroom(){
+window.location = "Bedroom.html"    
+}
+
+function livingRoom(){
+window.location = "Living room.html"    
+}
+
+function kitchen(){
+window.location = "Kitchen.html"    
+}
